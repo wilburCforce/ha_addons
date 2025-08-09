8a2201a6045a30f1c84c22c4a95d57905790ac9f
wilburCforce <109390391+wilburCforce@users.noreply.github.com>
2025-08-08 23:02:52 -0500

    dfg

--- ir_builder/run.sh
+++ ir_builder/run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/with-contenv bashio
 set -e
 bashio::log.info "Starting the IR Builder add-on..."
+bashio::log.info "Using python from: $(which python3)"
 if [ -z "$SUPERVISOR_TOKEN" ]; then
     bashio::log.fatal "SUPERVISOR_TOKEN is not set. Add-on cannot connect to the Supervisor API."
     exit 1
 else
     bashio::log.info "SUPERVISOR_TOKEN is successfully set. Add-on has access to the Supervisor API."
 fi
-exec python3 /app/app.py
+#exec python3 /app/app.py
+exec /opt/venv/bin/python3 /app/app.py
